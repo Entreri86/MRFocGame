bd8572f38c34057d48822a4689e3e10387a3db7c
Entreri86 <otal.fernando@gmail.com>
2017-06-01 09:32:14 +0200

    Despues de añadir la tortuga y las colisiones con esta

---
+++ core/src/com/foc/pmdm/game/Screens/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.foc.pmdm.game.Screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by entreri on 1/06/17.
+ */
+
+public class SplashScreen implements Screen {
+    private SpriteBatch batch;
+    private Sprite splash;
+    //private TweenManager tweenManager;
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
